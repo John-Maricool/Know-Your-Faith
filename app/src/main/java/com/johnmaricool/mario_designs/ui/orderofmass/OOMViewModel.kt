8eb1cd571cf789d035159618744a43fa3fd53958
JOHN BATISTA <ebukaiikenwa0902@gmail.com>
2021-12-03 07:57:40 -1100

    I removed some unused code and biloplate code in the project. I also implemented more architectural patterns in the app to reduce memory usage. I also totally changed the UI design of the app

--- app/src/main/java/com/johnmaricool/mario_designs/ui/orderofmass/OOMViewModel.kt
+++ app/src/main/java/com/johnmaricool/mario_designs/ui/orderofmass/OOMViewModel.kt
@@ -1,6 +1,7 @@
 package com.johnmaricool.mario_designs.ui.orderofmass
 
 import android.content.Context
+import android.text.Html
 import androidx.core.text.HtmlCompat
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
@@ -8,15 +9,15 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 
 class OOMViewModel
     @ViewModelInject
-    constructor(@ApplicationContext val context: Context)
+    constructor(@ApplicationContext var context: Context)
     : ViewModel() {
 
     fun getOOM(title: String): String {
-      val inputStream = context.assets.open("MY FILES/$title.txt")
+      val inputStream = context.assets.open("MY FILES/$title.html")
        val size: Int = inputStream.available()
        val buffer = ByteArray(size)
        inputStream.read(buffer)
-           val string =  HtmlCompat.fromHtml(String(buffer),  HtmlCompat.FROM_HTML_MODE_LEGACY)
+           val string =  HtmlCompat.fromHtml(String(buffer), 0)
        return string.toString()
     }
 }
